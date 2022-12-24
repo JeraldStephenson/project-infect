3b35a027e3b7995c7fc272f688a1a5b3a3fc90f5
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2022-12-23 16:25:40 -0800

    Initial Commit

---
+++ project-infect/project_infectApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_infectApp.swift
+//  project-infect
+//
+//  Created by Jerald Stephenson on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct project_infectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

58cb4c6dfc80de22c8428e2287466b630a73d1a9
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2022-12-31 15:06:56 -0800

    randomized half of the gameboard and mirrored it for player 2 to create fair play. un-randomized start points of players so they are not spreading zombieVirus before the first turn

---
+++ project-infect/Dev Notes.md
@@ -0,0 +1,4 @@
+#  Dev Notes
+
+If whole board is completely randomized - it can lead to unfair advantage from one player over another, so we will randomize half the board and then mirror it so that both sides are same
+
--- project-infect/GameBoard.swift
+++ project-infect/GameBoard.swift
@@ -25,6 +25,29 @@ class GameBoard: ObservableObject {
             
             for col in 0..<columnCount {
                 let zombieVirus = ZombieVirus(row: row, col: col)
+                
+                //logic to randomize half the board and mirror so that it is fair (fully randomized creates unfair opportunity during play)
+                if row <= rowCount / 2 {
+                    if row == 0 && col == 0 {
+                        //make sure the player starts pointing away from anything
+                        zombieVirus.direction = .north
+                    } else if row == 0 && col == 1 {
+                        //make sure nothing points to the player
+                        zombieVirus.direction = .east
+                    } else if row == 1 && col == 0 {
+                        //make sure nothing points to the player
+                        zombieVirus.direction = .south
+                    } else {
+                        //all other pieces are random
+                        zombieVirus.direction = ZombieVirus.Direction.allCases.randomElement()!
+                    }
+                } else {
+                    //mirror the top half of board (randomized) to the bottom half so that it is inverted and equal for player 2
+                    if let counterpart = getZombieVirus(atRow: rowCount - 1 - row, col: columnCount - 1 - col) {
+                        zombieVirus.direction = counterpart.direction.opposite
+                    }
+                }
+                
                 newRow.append(zombieVirus)
             }
             
@@ -33,7 +56,13 @@ class GameBoard: ObservableObject {
         
         grid[0][0].color = .green
         grid[rowCount - 1][columnCount - 1].color = .red
-        
-        
+    }
+    
+    func getZombieVirus(atRow row: Int, col: Int) -> ZombieVirus? {
+        guard row >= 0 else {return nil}
+        guard row < grid.count else { return nil }
+        guard col >= 0 else { return nil }
+        guard col < grid[0].count else {return nil}
+        return grid[row][col]
     }
 }
